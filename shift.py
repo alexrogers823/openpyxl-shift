c8b02f5fe8f968f5f936a87c0dc2814b1f005f10
Alex Rogers <alex.rogers823@gmail.com>
2018-09-30 13:08:33 -0400

    Fixed bugs and cleaned up for loops

--- shift.py
+++ shift.py
@@ -1,5 +1,5 @@
 import openpyxl
-from openpyxl.cell import get_column_letter
+from openpyxl.utils import get_column_letter
 
 # This class works with openpyxl to add/delete rows and columns in Excel
 # Openpyxl didn't have this feature to my knowledge, so I coded a way to do it
@@ -11,42 +11,48 @@ class Shift:
         self.row = worksheet.max_row
 
     def insert_row(self): # Inserts row
-        k = self.row
-        for i in range(self.selected, self.row+1):
+        for i in range(self.row, self.selected-1, -1):
             for j in range(1, self.column+1):
-                if not self.worksheet.cell(row=k, column=j).value:
-                    self.worksheet.cell(row=k+1, column=j) = ''
+                if not self.worksheet.cell(row=i, column=j).value:
+                    self.worksheet[get_column_letter(j)+str(i+1)] = None
                 else:
-                    shift_here = self.worksheet.cell(row=k, column=j).value
-                    self.worksheet.cell(row=k+1, column=j) = str(shift_here)
-            k -= 1
+                    shift_here = self.worksheet.cell(row=i, column=j).value
+                    self.worksheet[get_column_letter(j)+str(i+1)] = str(shift_here)
+                if i == self.selected:
+                    self.worksheet[get_column_letter(j)+str(i)] = None
+
 
     def insert_column(self): # Inserts column
-        k = self.column
-        for i in range(self.selected, self.column+1):
+        for i in range(self.column, self.selected-1, -1):
             for j in range(1, self.row+1):
-                if not self.worksheet.cell(row=j, column=self.column).value:
-                    self.worksheet.cell(row=j, column=self.column) = ''
+                if not self.worksheet.cell(row=j, column=i).value:
+                    self.worksheet[get_column_letter(i)+str(j)] = ''
                 else:
-                    shift_here = self.worksheet.cell(row=j, column=self.column).value
-                    self.worksheet.cell(row=j, column=self.column+1) = str(shift_here)
-            k -= 1
+                    shift_here = self.worksheet.cell(row=j, column=i).value
+                    self.worksheet[get_column_letter(i+1)+str(j)] = str(shift_here)
+                if i == self.selected:
+                    self.worksheet[get_column_letter(i)+str(j)] = None
+
 
     def remove_row(self): # Deletes row
-        for i in range(self.selected, self.row+1):
+        for i in range(self.row, self.selected-1, -1):
             for j in range(1, self.column+1):
                 if not self.worksheet.cell(row=i+1, column=j).value:
-                    self.worksheet.cell(row=i, column=j) = ''
+                    self.worksheet[get_column_letter(j)+str(i)] = None
                 else:
                     shift_here = self.worksheet.cell(row=i+1, column=j).value
-                    self.worksheet.cell(row=i, column=j) = str(shift_here)
+                    self.worksheet[get_column_letter(j)+str(i)] = str(shift_here)
+                if i == self.row-1:
+                    self.worksheet[get_column_letter(j)+str(i+1)] = None
 
 
     def remove_column(self): # Deletes column
-        for i in range(self.selected, self.row+1):
-            for j in range(1, self.column+1):
+        for i in range(self.column-1, self.selected-1, -1):
+            for j in range(1, self.row+1):
                 if not self.worksheet.cell(row=j, column=i+1).value:
-                    self.worksheet.cell(row=j, column=i) = ''
+                    self.worksheet[get_column_letter(i)+str(j)] = None
                 else:
                     shift_here = self.worksheet.cell(row=j, column=i+1).value
-                    self.worksheet.cell(row=j, column=i) = str(shift_here)
+                    self.worksheet[get_column_letter(i)+str(j)] = str(shift_here)
+                if i == self.column-1:
+                    self.worksheet[get_column_letter(i+1)+str(j)] = None
